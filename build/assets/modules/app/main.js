2f4a49229dea4fa506d0e3af763e80442bafa981
Dave Seah <david@davidseah.com>
2016-03-28 18:10:14 -0400

    base-global-require-config: create the global SYS1401 object, then add R_CONFIG to store the global path configuration for RequireJS, so we can extend it later without touching main.js

--- build/assets/modules/app/main.js
+++ build/assets/modules/app/main.js
@@ -1,39 +1,47 @@
-﻿requirejs.config({
-    baseUrl: '/modules',
-    paths: {
-        'text': 'vendor/require/text',
-        'durandal':'vendor/durandal/js',
-        'plugins' : 'vendor/durandal/js/plugins',
-        'transitions' : 'vendor/durandal/js/transitions',
-        'knockout': 'vendor/knockout/knockout',
-        'bootstrap': 'vendor/bootstrap/js/bootstrap.min',
-        'jquery': 'vendor/jquery/jquery.min'
-    },
-    shim: {
-        'bootstrap': {
-            deps: ['jquery'],
-            exports: 'jQuery'
-       }
-    }
-});
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
-    //>>excludeStart("build", true);
-    system.debug(true);
-    //>>excludeEnd("build");
+﻿if (window.SYS1401) {
+    console.error('Global SYS1401 object is already defined! Aborting.');
+} else {
+    window.SYS1401 = {};
+    SYS1401.R_CONFIG = {
+        baseUrl: '/modules',
+        paths: {
+            'text': 'vendor/require/text',
+            'durandal':'vendor/durandal/js',
+            'plugins' : 'vendor/durandal/js/plugins',
+            'transitions' : 'vendor/durandal/js/transitions',
+            'knockout': 'vendor/knockout/knockout',
+            'bootstrap': 'vendor/bootstrap/js/bootstrap.min',
+            'jquery': 'vendor/jquery/jquery.min'
+        },
+        shim: {
+            'bootstrap': {
+                deps: ['jquery'],
+                exports: 'jQuery'
+           }
+        }
+    };
+    requirejs.config(SYS1401.R_CONFIG);
+    define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+        //>>excludeStart("build", true);
+        system.debug(true);
+        //>>excludeEnd("build");
 
-    app.title = 'Engine1401A1';
+        app.title = 'Engine1401A1';
 
-    app.configurePlugins({
-        router: true,
-        dialog: true
+        app.configurePlugins({
+            router: true,
+            dialog: true
+        });
+
+        app.start().then(function() {
+            //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+            //Look for partial views in a 'views' folder in the root.
+            viewLocator.useConvention();
+
+            //Show the app by setting the root view model for our application with a transition.
+            app.setRoot('app/shell', 'entrance');
+        });
     });
+}
 
-    app.start().then(function() {
-        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-        //Look for partial views in a 'views' folder in the root.
-        viewLocator.useConvention();
 
-        //Show the app by setting the root view model for our application with a transition.
-        app.setRoot('app/shell', 'entrance');
-    });
-});
