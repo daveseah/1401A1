b41967e28dc24634f939fe45b838f8e2285ff891
Dave Seah <david@davidseah.com>
2016-03-28 20:28:10 -0400

    base-global-require-config: add SYS1401.AddModulePath() and SYS1401.UpdateModulePaths() to make addition of new modules doable outside of main.js

--- build/assets/modules/app/shell.js
+++ build/assets/modules/app/shell.js
@@ -1,9 +1,27 @@
-﻿define(['plugins/router', 'durandal/app'], function (router, app) {
+﻿/*//////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+    The Durandal application shell is loaded by /modules/app/main.js as an
+    object that defines object method related to routing. The routes,
+    defined in the activate: function, specify what modules to load in the
+    router.map() call. There is one module defined per route, which appears
+    in the shell's navigation menu. 
+
+    Note that shell.js is a Durandal "viewmodel", and has a corresponding
+    "view" in shell.html that is "bound" using KnockoutJS.
+ 
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * //////////////////////////////////////*/
+
+define([
+    'plugins/router', 
+    'durandal/app'
+], function (router, app) {
+    // return object that implements 
     return {
         router: router,
         search: function() {
             //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
+            //You can add custom options too. 
+            // Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         },
         activate: function () {
@@ -16,3 +34,6 @@
         }
     };
 });
+
+/// EXECUTION CONTINUES IN PATHS-TO-VIEWMODELS SPECIFIED BY MODULEID
+/// IN EACH ROUTE
